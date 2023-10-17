1a50b5cab1e3df8b007267e1a1eb37e9b79f4dd4
MJephat <mukundijephat22@gmail.com>
2023-10-17 14:27:43 +0300

    created signup model, Post method on signup

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,flash,request,redirect
 app = Flask(__name__)
+config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///ms.sdlite"
+app.config["SECRET_KEY"] = "my_secret_key"
+db=SQLAlchemy(app)
+
+# User Class
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    fname=db.Column(db.String(255), nullable=False)
+    lname=db.Column(db.String(255), nullable=False)
+    email=db.Column(db.String(255), nullable=False)
+    usernames=db.Column(db.String(255), nullable=False)
+    edu=db.Column(db.String(255), nullable=False)
+    password=db.Column(db.String(255), nullable=False)
+    status=db.Column(db.Integer, default=0, nullable=False)
+
+    def __repr__(self):
+        return f'User("{self.fname}","{self.lname}","{self.email}","{self.username}","{self.edu}","{self.password})'
+
+
 
 # main index 
 @app.route('/')
@@ -18,9 +37,24 @@ def userIndex():
     return render_template('user/index.html', title='User Login')
 
 # user register
-@app.route('/user/signup')
+@app.route('/user/signup', methods=['POST','GET'])
 def userSignup():
-    return render_template('user/signup.html', title='User Signup')
+    if request.methods=='POST':
+        #getting all inputs
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        email = request.form.get('email')
+        username = request.form.get('username')
+        edu = request.form.get('edu')
+        password = request.form.get('password')
+
+        # checking if all fields are filled or not.abs
+        if fname=="" or lname=="" or email=="" or username=="" or password=="" or edu=="":
+            flash('Please fill all the field','danger')
+            return redirect('/user/signup')
+        pass
+    else:
+        return render_template('user/signup.html', title='User Signup')
 
 
 
